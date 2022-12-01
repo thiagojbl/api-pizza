58b0251141f5d7516c9e75ebc17a750d0eba3097
Thiago José Barbosa Lima <thiagojb12@gmail.com>
2022-12-01 09:14:45 -0300

    API-REACT

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import { useEffect, useState } from 'react';
+import './App.css'; 
+import Prato from './componentes/Prato';
+
+function App() { 
+
+  const [pratos, setPratos] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:8000/api/pratos')
+      .then(resposta => resposta.json())
+      .then(dados => {
+        setPratos(dados)
+        console.log(dados) 
+      }) 
+  }, [])  
+  
+  return (
+    <div className="App">
+      <div className='banner-container'> 
+        <img src="/imagens/banner.png" alt="Logo banner"/> 
+      </div>
+      <div className='logo-container'>
+        <img src="/imagens/logo.png" alt="Logo Pizza"/> 
+      </div>
+      <section className='cardapio'> 
+        {pratos.map(prato => 
+        (<Prato 
+          key={prato.id} 
+          nome={prato.nome}  
+          descricao={prato.descricao} 
+          imagem={prato.imagem}
+          preco={prato.preco} 
+          />))} 
+      </section> 
+    </div>
+  ); 
+} 
+ 
+export default App;
---
+++ src/componentes/Prato.js
@@ -0,0 +1,27 @@
+import './Prato.css'
+
+const Prato = ({nome, descricao, imagem, preco}) => {
+    return (<div className='prato' >
+        <div className='imagem-container'> 
+            <img src={imagem} alt={descricao}/>
+        </div> 
+        
+        <div className='conteudo'>
+        
+            <p className='nome'>
+                <strong> {nome} </strong>
+            </p>
+
+            <p className='descricao'> 
+                {descricao}
+            </p>
+    
+            <p className='preco'>
+                <strong> {preco} </strong>
+            </p>
+        </div>
+
+    </div>)
+}
+
+export default Prato
